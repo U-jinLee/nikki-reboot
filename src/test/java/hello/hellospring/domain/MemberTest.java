3ef7d74777b12e67b60c72c1b3c54a0980886172
Yoojin Lee <yoojinLee.dev@gmail.com>
2021-12-30 00:17:18 +0900

    feat: MemberTest.java logic commit
    CreatedDate와 ModifiedDate에 테스트 로직 작성

--- src/test/java/hello/hellospring/domain/MemberTest.java
+++ src/test/java/hello/hellospring/domain/MemberTest.java
@@ -1,7 +1,35 @@
 package hello.hellospring.domain;
 
+import hello.hellospring.domain.Member.Member;
+import hello.hellospring.domain.Member.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDateTime;
 
-class MemberTest {
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+public class MemberTest {
 
+    @Autowired MemberRepository memberRepo;
+    @Test
+    public void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2021, 12, 29, 0, 0, 0);
+        Member member = new Member();
+        member.setId("member1");
+        Long no = memberRepo.save(member);
+        //when
+        Member member1 = memberRepo.findOne(no);
+        //then
+        System.out.printf("createdDate=%s, modifiedDate=%s",member1.getCreatedDate(), member1.getModifiedDate());
+        assertEquals(true, member1.getCreatedDate().isAfter(now));
+        assertEquals(true, member1.getModifiedDate().isAfter(now));
+    }
 }
